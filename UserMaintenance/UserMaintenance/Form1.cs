9f8dc8b635b8c249114f3e736b71876321258281
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-09-27 20:21:32 +0200

    Vált 6

--- UserMaintenance/UserMaintenance/Form1.cs
+++ UserMaintenance/UserMaintenance/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,14 @@ namespace UserMaintenance
             label1.Text = Resource1.FullName;
             
             button1.Text = Resource1.Add;
+            button2.Text = Resource1.WriteFile;
 
             listBox1.DataSource = users;
             listBox1.ValueMember = "ID";
             listBox1.DisplayMember = "FullName";
 
+
+            
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -36,6 +40,26 @@ namespace UserMaintenance
                 
             };
             users.Add(u);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                StreamWriter sw = new StreamWriter(sfd.FileName);
+
+                foreach (var u in users)
+                {
+                    sw.Write(u.ID);
+                    sw.Write(";");
+                    sw.Write(u.FullName);
+                    sw.WriteLine();
+                }
+
+
+                sw.Close();
+            }
         }
     }
 }
