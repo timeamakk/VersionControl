fb92eabaad7ea61918e61fa6a66d8a093f627d7b
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-10-07 17:34:45 +0200

    vált04_a

--- het05/het05/Form1.cs
+++ het05/het05/Form1.cs
@@ -12,9 +12,15 @@ namespace het05
 {
     public partial class Form1 : Form
     {
+        PortfolioEntities context = new PortfolioEntities();
+        List<Tick> Ticks;
+
         public Form1()
         {
             InitializeComponent();
+
+            Ticks = context.Ticks.ToList();
+            dataGridView1.DataSource = Ticks;
         }
     }
 }
