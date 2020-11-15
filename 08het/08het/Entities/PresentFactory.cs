e1260efd4808b22aa20bb707e6cec9fcc4d8b7b9
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-15 16:23:50 +0100

    v14a

---
+++ 08het/08het/Entities/PresentFactory.cs
@@ -0,0 +1,21 @@
+﻿using _08het.Entities;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08het.Abstractions
+{
+    public class PresentFactory : IToyFactory
+    {
+        public Color PresentColor { get; set; }
+
+        public Toy CreateNew()
+        {
+            return new Present(PresentColor);
+        }
+
+    }
+}
