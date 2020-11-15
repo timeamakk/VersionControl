42fee485209f38abeff65333314e5a1f6156c68c
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-15 10:59:13 +0100

    v2a

---
+++ 08het/08het/Entities/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _08het.Entities
+{
+    public class Ball : Label
+    {
+        public Ball()
+        {
+            AutoSize = false;
+            Width = 50;
+            Height = 50;
+            Paint += Ball_Paint;
+          
+        }
+
+        private void Ball_Paint(object sender, PaintEventArgs e)
+        {
+           
+        }
+    }
+}
