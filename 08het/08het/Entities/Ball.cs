532c08cfe526b1dbdc51e0f4a375f99dc7bfbbba
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-15 11:03:08 +0100

    v2b

--- 08het/08het/Entities/Ball.cs
+++ 08het/08het/Entities/Ball.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,7 +21,18 @@ namespace _08het.Entities
 
         private void Ball_Paint(object sender, PaintEventArgs e)
         {
-           
+            DrawImage(e.Graphics);
+        }
+
+        private void DrawImage(Graphics g)
+        {
+            g.FillEllipse(new SolidBrush(Color.Blue), 0, 0, Width, Height);
+        }
+
+
+        public void MoveBall()
+        {
+            Left += 1;
         }
     }
 }
