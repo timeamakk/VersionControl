411b9f915a69b877d1e39582aa82b6a5ece3015b
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-15 14:20:07 +0100

    v7

--- 08het/08het/Form1.cs
+++ 08het/08het/Form1.cs
@@ -43,7 +43,7 @@ namespace _08het
             var maxPosition = 0;
             foreach (var ball in _balls)
             {
-                ball.MoveBall();
+                ball.MoveToy();
                 if (ball.Left > maxPosition)
                     maxPosition = ball.Left;
             }
