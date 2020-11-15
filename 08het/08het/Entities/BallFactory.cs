9361fe5521363b4ab356120b94eefbca0ea41f5f
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-15 12:41:10 +0100

    v3

---
+++ 08het/08het/Entities/BallFactory.cs
@@ -0,0 +1,16 @@
+﻿  using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08het.Entities
+{
+    class BallFactory
+    {
+        public Ball CreatNew() {
+            return new Ball();    
+        }  
+
+    }
+}
