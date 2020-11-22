ab4820a7a4e5bc34c51b30c62a61feec2d642170
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-22 16:40:45 +0100

    v4

---
+++ UnitTestExample/AccountControllerTestFixture.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace UnitTestExample
+{
+    public class AccountControllerTestFixture
+    {
+
+        [Test]
+        public void TestValidateEmail(string email, bool expectedResult) { 
+        
+        
+        
+        }
+
+    }
+
+
+}
