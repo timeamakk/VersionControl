1ae1a443c762b9f5db31faebff6ab2bb446face6
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-22 17:44:21 +0100

    v7

---
+++ UnitTestExample/UnitTestExample.Test/AccountControllerTestFixture.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample
+{
+    public class AccountControllerTestFixture
+    {
+
+        [Test,
+        TestCase("abcd1234", false),
+        TestCase("irf@uni-corvinus", false),
+        TestCase("irf.uni-corvinus.hu", false),
+        TestCase("irf@uni-corvinus.hu", true)
+            ]
+        public void TestValidateEmail(string email, bool expectedResult) {
+
+            var accountController = new AccountController();
+
+            var actualResult = accountController.ValidateEmail(email);
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+
+    }
+
+
+}
---
+++ UnitTestExample/UnitTestExample/AccountControllerRegister.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnitTestExample.Controllers;
+
+namespace UnitTestExample.UnitTestExample
+{
+   public class AccountControllerRegister
+    {
+        [Test,
+        TestCase("abcdABCD", false),
+        TestCase("ABCD1234", false),
+        TestCase("abcd1234", false),
+        TestCase("abcd123", false),
+        TestCase("abCD1234", true),]
+        public void TestValidateRegister(string password, bool expectedResult)
+        {
+
+            var accountController = new AccountController();
+            var actualResult = accountController.ValidatePassword(password);
+
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+    }
+}
