4c1346c5a8257878ccb9503a40bb3255059d344f
Tímea Makk <timea.makk@stud.uni-corvinus.hu>
2020-11-25 22:18:13 +0100

    v5a

--- evolucio_10het/evolucio_10het/Form1.cs
+++ evolucio_10het/evolucio_10het/Form1.cs
@@ -36,6 +36,22 @@ namespace evolucio_10het
 
 
 
+            gc.GameOver += Gc_GameOver;
+
+            for (int i = 0; i < populationSize; i++)
+
+            { gc.AddPlayer(nbrOfSteps); }
+
+            gc.Start();
+
+
+        }
+
+        private void Gc_GameOver(object sender)
+        {
+            generation++;
+            label1.Text = string.Format( "{0}. generáció", generation);
+
         }
     }
 }
